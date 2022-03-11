7428cc11662f296a89db7a62470ffe31ee967c46
uschoen <uschoen@johjoh.de>
2022-03-11 20:06:46 +0100

    add ccu3 devices & channels

---
+++ eHCM/modul/homematic/devices/channels/HM_LC_Dim1L_CV_0.py
@@ -0,0 +1,30 @@
+'''
+Created on 11.03.2022
+@author: ullrich schoen
+
+'''
+
+__version__="8.0"
+__author__="ullrich schoen"
+
+# Standard library imports
+import logging
+# Local application imports
+from modul.hmc.devices.channels.defaultChannel import defaultChannel
+
+LOG=logging.getLogger(__name__)
+
+class HM_LC_Dim1L_CV_0(defaultChannel):
+
+    channel_type="HM_LC_Dim1L_CV_0"
+    channel_package="homematic"
+
+    def __init__(self,deviceID,channelCFG={},restore=False):
+
+        defaultChannel.__init__(self,deviceID,channelCFG,restore)
+
+
+
+        self.parameter['CFGVersion']=__version__
+
+        LOG.info("init channel %s finish, version %s, deviceID:%s"%(self.channel_type,self.channelName,__version__))
