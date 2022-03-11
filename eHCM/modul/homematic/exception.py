0aea92432631e54490e0e088fdf9be2c97c8749d
uschoen <uschoen@johjoh.de>
2022-03-11 17:24:22 +0100

    ccu3 exception handling

---
+++ eHCM/modul/homematic/exception.py
@@ -0,0 +1,24 @@
+'''
+Created on 01.12.2021
+
+@author: uschoen
+'''
+
+
+__version__='0.9'
+__author__ = 'ullrich schoen'
+
+# Standard library imports
+
+import logging
+
+LOG=logging.getLogger(__name__)
+
+class ccu3XML(Exception):
+    '''
+    
+    '''
+    def __init__(self, msg="unkown error occured",tracback=False):
+        super(ccu3XML, self).__init__(msg)
+        self.msg = msg
+        LOG.critical(msg,exc_info=tracback)
